edde93a1ccfdaf8bb7e7976efb722d2df4dada5a
Paul Cavallaro <ptc@fb.com>
2014-02-12 12:50:47 -0500

    Initial Commit

---
+++ OmNomNom/ONNAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  ONNAppDelegate.h
+//  OmNomNom
+//
+//  Created by Paul Cavallaro on 2/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ONNAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ OmNomNom/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  OmNomNom
+//
+//  Created by Paul Cavallaro on 2/12/14.
+//
+//
+
+#import <UIKit/UIKit.h>
+
+#import "ONNAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([ONNAppDelegate class]));
+    }
+}
